77d52ee5f6e8167c8393d9151d93e0ac1ee0752d
TymQa <125615116+TymQa@users.noreply.github.com>
2024-12-22 09:54:20 +0200

    learn .map

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const ideas = [
+		{
+			nick: 'cool-idea-nick-1',
+			name: 'Idea 1',
+			description: 'Description of idea 1...',
+		},
+		{
+			nick: 'cool-idea-nick-2',
+			name: 'Idea 2',
+			description: 'Description of idea 2...',
+		},
+		{
+			nick: 'cool-idea-nick-3',
+			name: 'Idea 3',
+			description: 'Description of ideaa 3...',
+		},
+		{
+			nick: 'cool-idea-nick-4',
+			name: 'Idea 4',
+			description: 'Description of ideaa 3...',
+		},
+		{
+			nick: 'cool-idea-nick-5',
+			name: 'Idea 5',
+			description: 'Description of ideaa 3...',
+		},
+	]
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+		<div>
+			<h1>Ideanick</h1>
+      {ideas.map((ideas) => {
+        return (
+          <div key={ideas.nick}>
+            <h2>{ideas.name}</h2>
+            <p>{ideas.description}</p>
+          </div>
+        )
+      })}
+		</div>
+	)
 }
 
 export default App
