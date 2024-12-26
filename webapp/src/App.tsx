6838a2383c1ca5c2e923090e8993e21eae854c7b
TymQa <125615116+TymQa@users.noreply.github.com>
2024-12-26 07:59:13 +0200

    +

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,37 +1,35 @@
-
-
 function App() {
   const ideas = [
-		{
-			nick: 'cool-idea-nick-1',
-			name: 'Idea 1',
-			description: 'Description of idea 1...',
-		},
-		{
-			nick: 'cool-idea-nick-2',
-			name: 'Idea 2',
-			description: 'Description of idea 2...',
-		},
-		{
-			nick: 'cool-idea-nick-3',
-			name: 'Idea 3',
-			description: 'Description of ideaa 3...',
-		},
-		{
-			nick: 'cool-idea-nick-4',
-			name: 'Idea 4',
-			description: 'Description of ideaa 3...',
-		},
-		{
-			nick: 'cool-idea-nick-5',
-			name: 'Idea 5',
-			description: 'Description of ideaa 3...',
-		},
-	]
+    {
+      nick: 'cool-idea-nick-1',
+      name: 'Idea 1',
+      description: 'Description of idea 1...',
+    },
+    {
+      nick: 'cool-idea-nick-2',
+      name: 'Idea 2',
+      description: 'Description of idea 2...',
+    },
+    {
+      nick: 'cool-idea-nick-3',
+      name: 'Idea 3',
+      description: 'Description of ideaa 3...',
+    },
+    {
+      nick: 'cool-idea-nick-4',
+      name: 'Idea 4',
+      description: 'Description of ideaa 3...',
+    },
+    {
+      nick: 'cool-idea-nick-5',
+      name: 'Idea 5',
+      description: 'Description of ideaa 3...',
+    },
+  ]
 
   return (
-		<div>
-			<h1>Ideanick</h1>
+    <div>
+      <h1>Ideanick</h1>
       {ideas.map((ideas) => {
         return (
           <div key={ideas.nick}>
@@ -40,8 +38,8 @@ function App() {
           </div>
         )
       })}
-		</div>
-	)
+    </div>
+  )
 }
 
 export default App
